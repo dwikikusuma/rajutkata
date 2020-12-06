4d4a1436b73c55b65be76e2fc57016621fce7982
dwikikusuma <71913640+dwikikusuma@users.noreply.github.com>
2020-12-06 16:03:16 +0700

    Add files via upload

---
+++ rajutkata.py
@@ -0,0 +1,25 @@
+
+def rajut(tes):
+    for i in range(len(tes)):
+       res = print(tes[:i+1],end="")
+    return res
+    
+
+print(rajut("Code"))
+
+# tes = "Code"
+
+
+# for i in range(len(tes)):
+#     print(tes[:i+1],end="")
+# tes1 = "CCoCodCode"
+# tmp = []
+# # #lompat 1 o lompat 2 d lompat 3 e
+# # coun = 2
+# i = 2
+# for i in range(len(tes1)):
+#     tmp.append(tes1[1::+1])
+#     # coun*=i
+# # i = i+1
+# # print(tmp[0::+2])
+# print(tmp)
